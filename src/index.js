bf87132b6a4b590a5cb6fd4c4a92f747ce954f05
nicknoi Tencent <chaiwat@tencent.co.th>
2019-08-11 00:01:02 +0700

    [master] connect to graphql-api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { GraphQLServer } from 'graphql-yoga'
+import get from 'lodash/get'
+import axios from 'axios'
 
 const port = 3000
 
@@ -10,7 +12,19 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
+    hello: async (_, { name }) => {
+      const result = await axios({
+        method: 'post',
+        url: 'http://localhost/',
+        data: {
+          query: "{ ping }"
+        }
+      })
+
+      const retMsg = get(result, 'data.data.ping')
+
+      return `hello ${retMsg}`
+    },
   },
 }
 

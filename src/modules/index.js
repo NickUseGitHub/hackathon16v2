765c1be3b426d3db0a1f561e443e28abeb349b6c
nicknoi Tencent <chaiwat@tencent.co.th>
2019-08-11 17:48:59 +0700

    [master] refactor to root modules

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,15 +1,10 @@
 import { GraphQLServer } from 'graphql-yoga'
+import path from 'path'
+import { fileLoader, mergeTypes } from 'merge-graphql-schemas'
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`
+const typesArray = fileLoader(path.join(__dirname, './**/schema.js'))
+const typeMergedDefs = mergeTypes(typesArray)
 
-const resolvers = {
-  Query: {
-    hello: () => 'hello world',
-  },
-}
+const resolvers = fileLoader(path.join(__dirname, './**/resolvers.js'))
 
-export default new GraphQLServer({ typeDefs, resolvers })
+export default new GraphQLServer({ typeDefs: typeMergedDefs, resolvers })

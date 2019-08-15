fda5dec739fac00ff1771376b4494f79f279718d
nicknoi Tencent <chaiwat@tencent.co.th>
2019-08-15 17:28:41 +0700

    [master] bring middleware back

--- src/modules/index.js
+++ src/modules/index.js
@@ -9,17 +9,17 @@ const typeMergedDefs = mergeTypes(typesArray)
 const resolvers = fileLoader(path.join(__dirname, './**/resolvers.js'))
 
 const middlewares = [
-  // function logger(resolve, parent, args, ctx, info) {
-  //   const reqHeaders = get(ctx, 'req.request.headers')
-  //   const ip = get(reqHeaders, 'x-forwarded-for')
-  //   const reqUrl = get(reqHeaders, 'origin')
-  //   const userAgent = get(reqHeaders, 'user-agent')
-  //   console.log('------ incoming info ------')
-  //   console.log('ip:', ip)
-  //   console.log('reqUrl:', reqUrl)
-  //   console.log('userAgent:', userAgent)
-  //   return resolve(parent, args, ctx, info)
-  // },
+  function logger(resolve, parent, args, ctx, info) {
+    const reqHeaders = get(ctx, 'req.request.headers')
+    const ip = get(reqHeaders, 'x-forwarded-for')
+    const reqUrl = get(reqHeaders, 'origin')
+    const userAgent = get(reqHeaders, 'user-agent')
+    console.log('------ incoming info ------')
+    console.log('ip:', ip)
+    console.log('reqUrl:', reqUrl)
+    console.log('userAgent:', userAgent)
+    return resolve(parent, args, ctx, info)
+  },
 ]
 
 export default new GraphQLServer({
